100ba16a0a54d9f91b6a9f0f1e045750c5013e40
Gedr <gimaev.ufa@gmail.com>
2022-06-09 21:23:05 +0500

    initial commit for lesson 23

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrgem install sinatra-reloaderap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
